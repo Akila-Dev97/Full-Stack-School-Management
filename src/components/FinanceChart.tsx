67fd30a8c7816b35495517f0d8ebddf896825ff1
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-31 22:43:35 +0530

    Style chart and finalize rendering

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -12,23 +12,69 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-// Finance chart data
 const data = [
-  { name: "Jan", income: 4000, expense: 2400 },
-  { name: "Feb", income: 3000, expense: 1398 },
-  { name: "Mar", income: 2000, expense: 9800 },
-  { name: "Apr", income: 2780, expense: 3908 },
-  { name: "May", income: 1890, expense: 4800 },
-  { name: "Jun", income: 2390, expense: 3800 },
-  { name: "Jul", income: 3490, expense: 4300 },
-  { name: "Aug", income: 3490, expense: 4300 },
-  { name: "Sep", income: 3490, expense: 4300 },
-  { name: "Oct", income: 3490, expense: 4300 },
-  { name: "Nov", income: 3490, expense: 4300 },
-  { name: "Dec", income: 3490, expense: 4300 },
+  {
+    name: "Jan",
+    income: 4000,
+    expense: 2400,
+  },
+  {
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: "Mar",
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: "Apr",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "May",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Jun",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Jul",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Aug",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Sep",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Oct",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Nov",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Dec",
+    income: 3490,
+    expense: 4300,
+  },
 ];
 
-// FinanceChart component
 const FinanceChart = () => {
   return (
     <div className="bg-white rounded-xl w-full h-full p-4">
@@ -67,6 +113,18 @@ const FinanceChart = () => {
             align="center"
             verticalAlign="top"
             wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px" }}
+          />
+          <Line
+            type="monotone"
+            dataKey="income"
+            stroke="#C3EBFA"
+            strokeWidth={5}
+          />
+          <Line
+            type="monotone"
+            dataKey="expense"
+            stroke="#CFCEFF"
+            strokeWidth={5}
           />
         </LineChart>
       </ResponsiveContainer>
