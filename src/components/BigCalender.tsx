2f3546623e2356e9b0c02de5a88013eb71d738a2
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-31 21:45:07 +0530

    Add view change handling and calendar customization

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,4 +1,6 @@
-import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+"use client";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
@@ -10,13 +12,24 @@ const BigCalendar = ({
 }: {
   data: { title: string; start: Date; end: Date }[];
 }) => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
   return (
     <Calendar
       localizer={localizer}
       events={data}
       startAccessor="start"
       endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
       style={{ height: "98%" }}
+      onView={handleOnChangeView}
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 17, 0, 0)}
     />
   );
 };
